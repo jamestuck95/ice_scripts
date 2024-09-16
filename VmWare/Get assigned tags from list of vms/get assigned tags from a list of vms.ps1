cd1c4a5f379023f4f2058bd661b1c796e08ca1a0
Tucky95 <jamest95@live.co.uk>
2024-09-16 16:32:57 +0100

    .

---
+++ VmWare/Get assigned tags from list of vms/get assigned tags from a list of vms.ps1
@@ -0,0 +1,44 @@
+﻿# Connect to vmware variables
+$Server = "end-epvc01.next-uk.next.loc"
+$creds = Get-StoredCredential -Target 'next'
+#Set-PowerCLIConfiguration -InvalidCertificateAction "ignore"
+
+
+
+         ################## Unhash the below to connect to vmware ############
+
+        #Connect-VIServer -Server $Server -Protocol https -Credential $creds
+            
+        ##################### Will prompt for DUO ###########################
+
+
+
+$list = Get-Content "C:\CSV\all_epvh01_vms.txt"
+
+$output_file = "C:\CSV\all_epvh01_tag_assignments.txt"
+
+
+$foreach = foreach ($vm in $list) {  
+
+
+        $get_tags = Get-TagAssignment -Entity $vm | select -ExpandProperty Tag -ErrorAction Ignore
+
+           $not_backed_up = if ( $get_tags -eq $null ) { echo "$vm is_not_backed_up_by_rubrik" }
+
+                
+                if ( $get_tags -eq $null ) { Write-Host "$vm is not backed up by rubrik" -ForegroundColor Yellow }
+
+                    else { Write-Host "$vm is assigned to $get_tags" }
+
+                                   
+            @( [pscustomobject]@{ VmName = $vm ; RubrikTag = $get_tags ; NotBackedUpServers = $not_backed_up } )
+                                                                                      
+                                                                                                                               
+}
+    
+    Write-Host "Exporting results to txt file..." -BackgroundColor White -ForegroundColor Blue
+
+        $foreach | FT -AutoSize | Out-File $output_file -Append
+
+
+ #  Get-TagAssignment -Entity "END-EDAS04" -ErrorAction Ignore
---
+++ VmWare/Get vm host and lun location/get vm host and lun location.ps1
@@ -0,0 +1,17 @@
+﻿
+# DRS variables
+$list = Get-Content "C:\CSV\edas_vms_list.txt"
+
+
+
+$pre_run_report = Foreach ($vm in $list) {
+
+        $get_vm = get-vm -Name $vm | select Name, VMHost
+
+        $get_lun = Get-HardDisk -VM $vm | select Filename
+
+       
+            @( [pscustomobject]@{ VmName = $get_vm.Name ; VmHost = $get_vm.VMHost ; LUN = $get_lun.Filename } )
+
+                                                 
+        } $pre_run_report | FT -AutoSize #| Out-File "c:\csv\pre_run_report.txt" -Append
