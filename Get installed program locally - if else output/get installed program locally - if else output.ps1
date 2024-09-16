239f59061d2b65508aed983b56585dd601c3fafc
Tucky95 <jamest95@live.co.uk>
2024-09-16 10:20:41 +0100

    add 2 folders

---
+++ Get installed program locally - if else output/get installed program locally - if else output.ps1
@@ -0,0 +1,10 @@
+﻿
+#$name = "END-EPWS256"
+
+
+
+$programs = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like "FireEye*"}
+
+if ( $programs -match "FireEye Endpoint Agent" ) { Write-Host 'FireEye is installed on ....' -ForegroundColor Green }
+
+else { Write-Host 'FireEye is NOT installed on .....' -ForegroundColor Yellow }
---
+++ Remotely view and logoff user session/remotely view and logoff user session.ps1
@@ -0,0 +1,8 @@
+﻿
+#Get-WmiObject –ComputerName "END-EPAS312" –Class Win32_ComputerSystem | Select-Object *
+
+# 1st command shows logged in users & sessionID
+Invoke-Command -ComputerName 'END-EPAS312.next-uk.next.loc' -ScriptBlock { quser }
+
+#2nd command logs off user using session ID - change number after 1 to user's sessionid
+Invoke-Command -ComputerName 'END-EPAS312.next-uk.next.loc' -ScriptBlock { logoff 1 }
