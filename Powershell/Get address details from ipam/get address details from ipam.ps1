b15185a41658655dc27666950dad126e426c1bb3
Tucky95 <jamest95@live.co.uk>
2024-09-20 10:17:38 +0100

    ipam scripts

---
+++ Powershell/Get address details from ipam/get address details from ipam.ps1
@@ -0,0 +1,22 @@
+﻿
+# Jita access to IPAM server. Will prompt for 2FA code
+    
+ #####   C:\BulkJita\bulk_jita_next.ps1 -JITA END-IPDI09
+
+
+ $computerName = "END-IPDI09.next-uk.next.loc"
+
+# Will create text box to enter IP address. Pre-populates with 172.28.
+
+    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+
+    $ip = [Microsoft.VisualBasic.Interaction]::InputBox('Enter a IP address' , 'IPAM Lookup' , '172.28.')
+
+
+#$ip = "172.28.193.215"
+
+# Uses winrm to run commands remotely against ipam server. Will need to add reg key to intune/AAD laptop if haven't already:
+## reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WSMAN\Client /v trusted_hosts /t REG_SZ /d "*.next-uk.next.loc" /f
+
+Invoke-Command -ComputerName $computerName -ScriptBlock { Get-IpamAddress -IpAddress $Using:ip | Select DeviceName, IpAddress, Description, AssignmentDate, ExpiryDate, IpAddressState, Owner, AssignmentType, ExpiryStatus, ManagedByService } | Out-GridView
+
---
+++ Powershell/Get address details from ipam/search ipam from range.ps1
@@ -0,0 +1,47 @@
+﻿# Jita access to IPAM server. Will prompt for 2FA code
+    
+ #####   C:\BulkJita\bulk_jita_next.ps1 -JITA END-IPDI09
+
+
+$computerName = "END-IPDI09.next-uk.next.loc"
+
+
+$iprange = 56..75
+    #This variable represents the 4th octet of the IP address. Change this to anyting between 0-254 depending on how much of the range you want to search
+
+$search_ipam = Foreach ($ip in $iprange){
+
+    $range = '101.10.155.'
+        #This is the first 3 octets of the ip address. Will combine with the first variable to conduct the search. Change this line to search different subnets
+    $computer = $range + $ip
+
+
+
+    if ( $i = Invoke-Command -ComputerName $computerName -ScriptBlock { Get-IpamAddress -IpAddress $Using:computer } ) {
+    
+    $device = $i.DeviceName
+    $ipaddr = $i.IPv4Address
+    $ipstate = $i.IPAddressState
+    $desc = $i.Description
+    $owner = $i.Owner
+    $assignmentd = $i.AssignmentDate
+    
+
+        Write-Host "$computer exists in ipam as $device - IP_State = $ipstate - Description = $desc - Owner = $owner - Assigned_Date = $assignmentd " -ForegroundColor Yellow  -BackgroundColor Black
+               
+
+    } 
+
+        else {
+        
+                Write-Output "$computer is not in IPAM" #| Out-file C:\SCRIPTS\CSV_s\next_csv\free_ips_155vlan2.txt -Append
+                    #This line works to export to file
+
+            
+}
+    }
+
+# Invoke-Command -ComputerName $computerName -ScriptBlock { Get-IpamAddress -IpAddress 101.10.155.56 | Select DeviceName, IpAddress, Description, AssignmentDate, ExpiryDate, IpAddressState, Owner, AssignmentType, ExpiryStatus, ManagedByService }
+
+
+#C:\SCRIPTS\CSV_s\next_csv\free_ips_155vlan2.txt
